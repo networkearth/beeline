0e332cedc4b3f8c22499a43490688c1448802688
erebuseternal <marcelsanders96@gmail.com>
2025-04-11 19:55:39 +0000

    wip

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     packages=find_packages(include=["beeline", "beeline*"]),
     install_requires=[
         "click==8.1.7",
+        "boto3==1.35.50",
     ],
     entry_points={
         "console_scripts": [
